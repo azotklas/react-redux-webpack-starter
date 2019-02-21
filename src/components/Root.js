9617da955c0deb175c9f993d5c941b5bf7e4b52c
Denis Pytliak <Azot952@gmail.com>
2019-02-21 11:47:03 +0300

    working project EMPTY

---
+++ src/components/Root.js
@@ -0,0 +1,10 @@
+
+import React, { Component } from 'react';
+import '../styles/Root.css';
+const Root = ({}) => (
+    <div id='container'>
+       <h1>test</h1>
+    </div>
+)
+
+export default Root
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Root from "./components/Root.js";
+import './styles/Root.css';
+
+ReactDOM.render(
+    <Root />,
+    document.getElementById('root')
+)
+    
