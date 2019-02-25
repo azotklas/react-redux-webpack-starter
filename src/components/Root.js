3b64811bbf7a75d74d21865977d7a3d54d330cf5
Denis Pytliak <Azot952@gmail.com>
2019-02-26 00:36:07 +0300

    add JSS (react-jss)

--- src/components/Root.js
+++ src/components/Root.js
@@ -2,13 +2,31 @@
 import React, { Component } from 'react';
 import SearchContainer from '../containers/SearchContainer'
 import ResultContainer from '../containers/ResultContainer'
+// Import React-JSS
+import injectSheet from 'react-jss';
 import '../styles/Root.css';
 
-const Root = ({}) => (
-    <div id='container'>
-       <h1>test text</h1>
-       <SearchContainer />
-       <ResultContainer />
-    </div>
-)
-export default Root
+const styles = {
+    wrapper: {
+        backgroundColor: '#fafafa',
+        padding: '60px 0',
+    },
+};
+
+class Root extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        const { classes } = this.props;
+        return (
+            <div id='container' className={classes.wrapper}>
+                <h1>test text</h1>
+                <SearchContainer />
+                <ResultContainer />
+            </div>
+        )
+    }
+}
+
+export default injectSheet(styles)(Root) 
