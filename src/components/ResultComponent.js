2502c133e20085c6a4830d72f70ce535885a5d40
Denis Pytliak <Azot952@gmail.com>
2019-07-28 21:42:42 +0300

    add material-ui and version changed for react and react-dom

--- src/components/ResultComponent.js
+++ src/components/ResultComponent.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+// Import React-JSS
+import injectSheet from 'react-jss';
+import '../styles/Root.css';
 
-const ResultComponent = ({}) => (
-    <div id='container'>
-       <h1>ResultComponent</h1>
-    </div>
-)
-export default ResultComponent
+const styles = {
+    wrapper: {
+        backgroundColor: '#fafafa',
+        padding: '60px 0',
+    },
+};
+
+class ResultComponent extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        const { classes } = this.props;
+        return (
+            <div id='container' className={classes.wrapper}>
+                <Button variant="contained" color="primary">
+                    ResultComponent
+                </Button>
+            </div>
+        )
+    }
+}
+
+export default injectSheet(styles)(ResultComponent) 
